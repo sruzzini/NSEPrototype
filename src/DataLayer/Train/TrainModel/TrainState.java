dbd4f4030b163337b9b069b0d31298879bad8c2c
Drew Winfield <daw106@pitt.edu>
2014-03-28 15:51:45 -0400

    Put source code in PhysicsEngine.java and TrainState.java, TrainModel.java still empty

--- src/DataLayer/Train/TrainModel/TrainState.java
+++ src/DataLayer/Train/TrainModel/TrainState.java
@@ -12,4 +12,87 @@ package DataLayer.Train.TrainModel;
  */
 public class TrainState {
     
+    private boolean rightDoors;
+    private boolean leftDoors;
+    private boolean intLights;
+    private boolean extLights;
+    private String announcement;
+    private boolean heater;
+    private int temperature;
+    
+    public TrainState()
+    {
+        rightDoors = false;
+        leftDoors = false;
+        intLights = false;
+        extLights = false;
+        announcement = "";
+        heater = false;
+        temperature = 65;
+    }
+    
+    // right doors
+    public void setRightDoors(boolean state)
+    {
+        rightDoors = state;
+    }
+    public boolean getRightDoors()
+    {
+        return rightDoors;
+    }
+    // left doors
+    public void setLeftDoors(boolean state)
+    {
+        leftDoors = state;
+    }
+    public boolean getLeftDoors()
+    {
+        return leftDoors;
+    }
+    // interior lights
+    public void setInteriorLights(boolean state)
+    {
+        intLights = state;
+    }
+    public boolean getInteriorLights()
+    {
+        return intLights;
+    }
+    // exterior lights
+    public void setExteriorLights(boolean state)
+    {
+        extLights = state;
+    }
+    public boolean getExteriorLights()
+    {
+        return extLights;
+    }
+    // announcement
+    public void setAnnouncement(String newAnnouncement)
+    {
+        announcement = newAnnouncement;
+    }
+    public String getAnnouncement()
+    {
+        return announcement;
+    }
+    // heater
+    public void setHeater(boolean state)
+    {
+        heater = state;
+    }
+    public boolean getHeater()
+    {
+        return heater;
+    }
+    // temperature
+    public void setTemperature(int newTemp)
+    {
+        temperature = newTemp;
+    }
+    public int getTemperature()
+    {
+        return temperature;
+    }
+    
 }
