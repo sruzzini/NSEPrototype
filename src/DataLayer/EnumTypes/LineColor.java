754d2a16bc49abc46244ded49fe6a214fb9fbfc4
Nathan Hachten <nwh4@pitt.edu>
2014-03-30 12:31:50 -0400

    Added .enum files for light color, line color, and railroad crossing state.

---
+++ src/DataLayer/EnumTypes/LineColor.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package DataLayer.EnumTypes;
+
+/**
+ * This enumerated type provides colors for each train line.
+ * 
+ * @author nwhachten
+ * @version 1.0
+ * @since 2014-03-30
+ *
+ */
+public enum LineColor {
+    RED, GREEN;
+    
+}
---
+++ src/DataLayer/EnumTypes/XingState.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package DataLayer.EnumTypes;
+
+/**
+ * This enumerated type provides status for railroad crossings on a block.
+ *
+ * @author nwhachten
+ * @version 1.0
+ * @since 2014-03-30
+ * 
+ */
+public enum XingState {
+    NO_XING_ON_BLOCK, ACTIVE, NOT_ACTIVE;
+    
+}
