2fcd6a86e946bb4befdbb421974f8faa3d46a5d1
sruzzini <ruzzinis@gmail.com>
2014-04-11 15:45:23 -0400

    Updated: BeaconSignal

--- src/DataLayer/TrackModel/BeaconSignal.java
+++ src/DataLayer/TrackModel/BeaconSignal.java
@@ -2,34 +2,24 @@ package DataLayer.TrackModel;
 
 public class BeaconSignal 
 {
-    private int beaconID;
-    private String signal;
-    
-    public BeaconSignal(int beaconID, String signal) 
-    {
-        this.beaconID = beaconID;
-        this.signal = signal;
-    }
-
-    public int getBeaconID() 
-    {
-        return beaconID;
-    }
-
-    public void setBeaconID(int beaconID) 
-    {
-        this.beaconID = beaconID;
-    }
-
-    public String getSignal() 
-    {
-        return signal;
-    }
-
-    public void setSignal(String signal) 
-    {
-        this.signal = signal;
-    }
+    public String StationName;
+    public boolean StationOnRight;
+    public double Velocity;
     
     
+    //Constructors
+    
+    public BeaconSignal()
+    {
+        this.StationName = "";
+        this.Velocity = 0;
+        this.StationOnRight = true;
+    }
+    
+    public BeaconSignal(String station, double velocity, boolean stationOnRight) 
+    {
+        this.StationName = station;
+        this.Velocity = velocity;
+        this.StationOnRight = stationOnRight;
+    }
 }
