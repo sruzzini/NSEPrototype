02339f4274d1398388fab1addb9884b4c9f25a68
Ryan Mertz <rmertz92@gmail.com>
2014-04-21 12:58:31 -0400

    updated switch for coding standard

--- src/DataLayer/TrackModel/Switch.java
+++ src/DataLayer/TrackModel/Switch.java
@@ -4,13 +4,22 @@ import DataLayer.EnumTypes.LineColor;
 
 public class Switch 
 {
-    public LineColor lineID;
-    public int switchID;
-    public int approachBlock;
-    public int straightBlock;
-    public int divergentBlock;
-    public boolean straight;
+    public int approachBlock; // number of block that is the approach block of the switch
+    public int divergentBlock; // number of block that is the divergent block of the switch
+    public LineColor lineID; // line identifier for the line that the switch belongs to
+    public boolean straight; // boolean - is the switch in the straight or divergent position
+    public int straightBlock; // number of block that is the straight block of the switch
+    public int switchID; // switch identifier
     
+    // Switch(LineColor lineID, int switchID, int approachBlock, int straightBlock, int divergentBlock, boolean straight)
+    // instantiates a new switch object with the specified parameters
+    // Parameters:
+    //     LineColor lineID - line that the new switch belongs to
+    //     int switchID - identifier for the new switch
+    //     int approachBlock - identifier for the approach block
+    //     int straightBlock - identifier for the straight block
+    //     int divergentBlock - identifier for the divergent block
+    //     boolean straight - initial position of the switch upon instantiation
     public Switch(LineColor lineID, int switchID, int approachBlock, 
             int straightBlock, int divergentBlock, boolean straight)
     {
@@ -22,11 +31,18 @@ public class Switch
         this.straight = straight;
     }
     
+    // isStraight() returns whether or not the switch is in the straight position
+    // Returns - boolean, whether or not the switch is in the straight position
     public boolean isStraight() 
     {
         return straight;
     }
     
+    // matches(Switch s) returns whether or not this switch object has all the same 
+    // instance variable values of the switch passed in
+    // Parameters:
+    //     Switch s - the switch to be compared to this switch
+    // Returns - boolean, whether or not the switches match
     public boolean matches(Switch s)
     {
         boolean result = true;
@@ -37,6 +53,6 @@ public class Switch
             result = false;
         }
         
-        return false;
+        return result;
     }
 }
