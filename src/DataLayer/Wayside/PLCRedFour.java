fa2cecfbcd5620a36a4e38c515f10ef95d039b36
Nathan Hachten <nwh4@pitt.edu>
2014-04-23 16:32:05 -0400

    More logic for red line PLC

--- src/DataLayer/Wayside/PLCRedFour.java
+++ src/DataLayer/Wayside/PLCRedFour.java
@@ -6,6 +6,7 @@
 
 package DataLayer.Wayside;
 
+import DataLayer.Bundles.BlockSignalBundle;
 import DataLayer.EnumTypes.LineColor;
 import DataLayer.TrackModel.Block;
 import DataLayer.TrackModel.Switch;
@@ -21,6 +22,10 @@ public class PLCRedFour extends PLC {
     private int trainsGoing;        //headed out of plc dominion
     private boolean comingThru49;   //train on block 49 headed towards loop
     private boolean goingThru49;    //train on block 49 headed out of plc dominion
+    private boolean enteringLoop;
+    private boolean leavingLoop;
+    private boolean holdAt66;
+    
     
     public PLCRedFour(int id, LineColor line, HashMap routeTable) 
     {
@@ -30,6 +35,9 @@ public class PLCRedFour extends PLC {
         this.trainsInLoop = 0;
         this.comingThru49 = false;
         this.goingThru49 = false;
+        this.leavingLoop = false;
+        this.enteringLoop = false;
+        this.holdAt66 = false;
     }
     
     @Override
@@ -41,7 +49,10 @@ public class PLCRedFour extends PLC {
         Block block48 = this.blocks.get(48);
         Block block49 = this.blocks.get(49);
         Block block50 = this.blocks.get(50);
-        Switch switch7 = this.switches.get(7);
+        Block block53 = this.blocks.get(53);
+        Block block54 = this.blocks.get(54);
+        Block block66 = this.blocks.get(66);
+        Switch switch6 = this.switches.get(6);
         
         if (block49.isOccupied() && !block48.isOccupied() && !block50.isOccupied())
         {
@@ -72,6 +83,71 @@ public class PLCRedFour extends PLC {
                 trainsGoing--;
             }
         }
+        
+        if (block53.isOccupied() && !block54.isOccupied() && !block66.isOccupied())
+        {
+            if (trainsComing > 0)
+            {
+                enteringLoop = true;
+                holdAt66 = true;
+                leavingLoop = false;
+                boolean dir = false;
+                if (switch6.StraightBlock == 54) dir = true;
+                c.pushCommand(new Switch(LineColor.RED, switch6.SwitchID, 
+                        switch6.ApproachBlock, switch6.StraightBlock, 
+                        switch6.DivergentBlock, dir ));
+            }
+            else
+            {
+                if (leavingLoop)
+                {
+                    leavingLoop = false;
+                    trainsInLoop--;
+                    trainsGoing++;
+                }
+            }
+        }
+        
+        if (block54.isOccupied() && !block53.isOccupied())
+        {
+            if (enteringLoop)
+            {
+                enteringLoop = false;
+                trainsComing--;
+                trainsInLoop++;
+                if (trainsComing == 0)
+                {
+                    holdAt66 = false;
+                }
+            }
+        }
+        
+        if (block66.isOccupied() && !block53.isClosed())
+        {
+            if (trainsComing > 0)
+            {
+                holdAt66 = true;
+                leavingLoop = false;
+                //tell block 66 to stop
+                c.pushCommand(new BlockSignalBundle(block66.getAuthority(), block66.getDestination(), 0.0, 66, LineColor.RED));
+            }
+            else
+            {
+                if (holdAt66)
+                {
+                    c.pushCommand(new BlockSignalBundle(block66.getAuthority(), block66.getDestination(), block66.getSpeedLimit(), 66, LineColor.RED));
+                }
+                holdAt66 = false;
+                leavingLoop = true;
+                boolean dir = false;
+                if (switch6.StraightBlock == 66) dir = true;
+                c.pushCommand(new Switch(LineColor.RED, switch6.SwitchID, 
+                        switch6.ApproachBlock, switch6.StraightBlock, 
+                        switch6.DivergentBlock, dir ));
+            }
+        }
+        
+        
         
         return c;
       
--- src/DataLayer/Wayside/PLCRedOne.java
+++ src/DataLayer/Wayside/PLCRedOne.java
@@ -7,6 +7,8 @@
 package DataLayer.Wayside;
 
 import DataLayer.EnumTypes.LineColor;
+import DataLayer.TrackModel.Block;
+import DataLayer.TrackModel.Switch;
 import java.util.HashMap;
 
 /**
@@ -14,10 +16,26 @@ import java.util.HashMap;
  * @author nwhachten
  */
 public class PLCRedOne extends PLC{
+    private int trainsDeparting;
+    private int trainsReturning;
+    private int trainsComing;
+    private int trainsGoing;
+    private boolean leavingYard;
+    private boolean returningToYard;
+    private boolean leavingLoop;
+    private boolean enteringLoop;
     
     public PLCRedOne(int id, LineColor line, HashMap routeTable) 
     {
         super(id, line, routeTable);
+        this.trainsComing = 0;
+        this.trainsDeparting = 0;
+        this.trainsGoing = 0;
+        this.trainsReturning = 0;
+        this.leavingLoop = false;
+        this.leavingYard = false;
+        this.enteringLoop = false;
+        this.returningToYard = false;
     }
     
     @Override
@@ -25,6 +43,122 @@ public class PLCRedOne extends PLC{
     {
         Commands c;
         c = new Commands();
+        
+        Block block9 = this.blocks.get(9);
+        Block block77 = this.blocks.get(77);
+        Block block1 = this.blocks.get(1);
+        Block block15 = this.blocks.get(15);
+        Block block16 = this.blocks.get(16);
+        Block block10 = this.blocks.get(10);
+        Switch switch1 = this.switches.get(1);
+        Switch switch7 = this.switches.get(7);
+        
+        if (block77.isOccupied() && !block9.isOccupied())
+        {
+            if (returningToYard)
+            {
+                returningToYard = false;
+                trainsReturning--;
+            }
+            else
+            {
+                leavingYard = false;
+                boolean dir = false;
+                if (switch7.StraightBlock == 77) dir = true;
+                c.pushCommand(new Switch(LineColor.RED, switch7.SwitchID, 
+                        switch7.ApproachBlock, switch7.StraightBlock, 
+                        switch7.DivergentBlock, dir ));
+            }
+        }
+        
+        if (block9.isOccupied() && !block77.isOccupied())
+        {
+            if (leavingYard)
+            {
+                leavingYard = false;
+                trainsDeparting++;
+            }
+            else if (trainsDeparting == 0)
+            {
+                returningToYard = true;
+                boolean dir = false;
+                if (switch7.StraightBlock == 77) dir = true;
+                c.pushCommand(new Switch(LineColor.RED, switch7.SwitchID, 
+                        switch7.ApproachBlock, switch7.StraightBlock, 
+                        switch7.DivergentBlock, dir ));
+            }
+        }
+        
+        if (block1.isOccupied() && !block16.isOccupied())
+        {
+            if (trainsDeparting > 0)
+            {
+                leavingLoop = true;
+                boolean dir = false;
+                if (switch1.StraightBlock == 1) dir = true;
+                c.pushCommand(new Switch(LineColor.RED, switch1.SwitchID, 
+                        switch1.ApproachBlock, switch1.StraightBlock, 
+                        switch1.DivergentBlock, dir ));
+            }
+            else if (enteringLoop)
+            {
+                enteringLoop = false;
+                trainsComing--;
+                trainsReturning++;
+            }
+        }
+        
+        if (block16.isOccupied() && !block1.isOccupied())
+        {
+            if (leavingLoop)
+            {
+                leavingLoop = false;
+                trainsDeparting--;
+                trainsGoing++;
+            }
+            else if (trainsComing > 0)
+            {
+                enteringLoop = true;
+                if (trainsDeparting > 0)
+                {
+                    //set switch towards block 15
+                    boolean dir = false;
+                    if (switch1.StraightBlock == 15) dir = true;
+                    c.pushCommand(new Switch(LineColor.RED, switch1.SwitchID, 
+                        switch1.ApproachBlock, switch1.StraightBlock, 
+                        switch1.DivergentBlock, dir ));
+                    
+                }
+                else
+                {
+                    //set switch towards block 1
+                    boolean dir = false;
+                    if (switch1.StraightBlock == 1) dir = true;
+                    c.pushCommand(new Switch(LineColor.RED, switch1.SwitchID, 
+                        switch1.ApproachBlock, switch1.StraightBlock, 
+                        switch1.DivergentBlock, dir ));
+                }
+            }
+        }
+        
+        if (block10.isOccupied() && !block9.isOccupied())
+        {
+            boolean dir = false;
+            if (switch7.StraightBlock == 10) dir = true;
+                c.pushCommand(new Switch(LineColor.RED, switch7.SwitchID, 
+                        switch7.ApproachBlock, switch7.StraightBlock, 
+                        switch7.DivergentBlock, dir ));
+        }
+        
+        if (block15.isOccupied() && !block16.isOccupied())
+        {
+            if (enteringLoop)
+            {
+                enteringLoop = false;
+                trainsComing--;
+                trainsDeparting++;
+            }
+        }
         
         return c;
       
